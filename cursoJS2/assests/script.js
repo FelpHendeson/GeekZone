58ddba644db7ad09067992987acd33b7d11eda36
Felipe Costa <felipe.costa@ofm.com.br>
2023-11-13 17:21:37 -0300

    add => Termino da função de apagar tarefa e inicio do salvamento da lista

--- cursoJS2/assests/script.js
+++ cursoJS2/assests/script.js
@@ -23,17 +23,33 @@ function criaBtnDelete(li) {
     li.innerText += ' ';
     const btnDelete = document.createElement('button');
     btnDelete.innerText = 'Deletar';
+    btnDelete.setAttribute('class', 'delete');
+    btnDelete.setAttribute('title', 'Deleta está tarefa')
     li.appendChild(btnDelete);
-}
+};
 
 function criaTarefa(textoinput) {
   const li = criaLi();
   li.innerText = textoinput;
   tarefas.appendChild(li);
   limpaCampo();
-}
+  criaBtnDelete(li);
+  salvarTarefas();
+};
 
 btnTarefa.addEventListener("click", function () {
   if (!inputTarefa.value) return;
   criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+  if( el.classList.contains('delete')) {
+    el.parentElement.remove();
+  }
+});
+
+function salvarTarefas() {
+  const lista = tarefas.querySelectorAll('li');
+  
+};
