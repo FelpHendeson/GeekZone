64ac2dd57c97e9f3dafe4b5efddef1d3f3eb11b6
Felipe Costa <felipe.costa@ofm.com.br>
2023-12-06 17:22:39 -0300

    add => Construção de função assimcronba bruta

---
+++ cursoJS6/assets/scripts/script.js
@@ -0,0 +1,30 @@
+//frase = frase a ser dita || tempo = tempo até a frase ser dita
+function falaFrase(frase, tempo) {
+    return new Promise((resolve, reject) => {
+        if (typeof frase !== 'string') {
+            reject('Dado passado Incompativél')
+        };
+
+        setTimeout(() => {
+            resolve(frase);
+        }, tempo);
+    });
+}
+
+falaFrase('Instânciando conexão com Banco de Dados', 1600)
+    .then(resposta => {
+        console.log(resposta);
+        return falaFrase('Conexão com o Banco de Dados Bem-Sucedida', 1600);
+    })
+    .then(resposta => {
+        console.log(resposta);
+        return falaFrase(2004, 1600);
+    })
+    .then(() => {
+        console.log('Construa sua querry')
+    })
+    .catch(e => {
+        console.log('ERRO: ', e)
+    });
+
+console.log('Exemplo de Função assíncrona brutaaaaa!!!!!')
