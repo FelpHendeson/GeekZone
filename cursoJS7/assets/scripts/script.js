bd24983e3ca6f220440d32b677859389249e2f3a
Felipe Costa <felipe.costa@ofm.com.br>
2023-12-07 17:09:59 -0300

    add => Inicio de um projetnho proprio felas

---
+++ cursoJS7/assets/scripts/script.js
@@ -0,0 +1,13 @@
+// seleciona a DIV da tela em que eu quero gerar spontaneamente o formulario
+const btnFormLogin = document.querySelector('#btnFormLogin') ;
+const btnFormRegistro = document.querySelector('#btnFormRegistro') ;
+const divFormTeste = document.querySelector('.form-teste');
+
+// Adiciona o disparo para surgimento do formulario espontaneamente ao botão
+btnFormLogin.addEventListener('click', e => {
+
+});
+
+function geraFormulario(label, inptClass, inptId, inptName) {
+    
+}
